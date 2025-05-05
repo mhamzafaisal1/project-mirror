b4c1cfb9ee104392633cbdea808eedb386ac5e17
Hamza Faisal <hfaisal@ChiDry.local>
2025-05-05 15:38:21 -0500

    Created item summary table component

---
+++ chitrac-ng/src/app/item-summary-table/item-summary-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-summary-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-summary-table.component.html',
+  styleUrl: './item-summary-table.component.scss'
+})
+export class ItemSummaryTableComponent {
+
+}
