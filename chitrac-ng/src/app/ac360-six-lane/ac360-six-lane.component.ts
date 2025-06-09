af48b198e6b14ac2ec02433e5f4424b6d5e21813
Hamza Faisal <hfaisal@ChiDry.local>
2025-06-09 12:52:34 -0500

    Added sub menus to side nav bar

---
+++ chitrac-ng/src/app/ac360-six-lane/ac360-six-lane.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SixLaneFlipperComponent } from '../blanket-blaster/six-lane-flipper/six-lane-flipper.component';
+
+@Component({
+  selector: 'app-ac360-six-lane',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SixLaneFlipperComponent
+  ],
+  templateUrl: './ac360-six-lane.component.html',
+  styleUrl: './ac360-six-lane.component.scss'
+})
+export class AC360SixLaneComponent {
+  // Component is now empty as it just serves as a container for the six-lane-flipper
+}
