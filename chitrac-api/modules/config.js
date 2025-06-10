4425d340f88eb93b0a5f79e9fa7b95f5fc4cfed6
Hamza Faisal <hfaisal@ChiDry.local>
2025-06-10 08:03:24 -0500

    implemented .env configuration to get rid of config files

---
+++ chitrac-api/modules/config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+  nodeEnv: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT, 10) || 3000,
+
+  // MongoDB (Main App)
+  mongo: {
+    url: process.env.MONGO_URI,
+    db: process.env.MONGO_URI.split('/').pop() || 'chitrac'
+  },
+
+  // MongoDB (Winston Logging)
+  mongoLog: {
+    url: process.env.MONGO_LOG_URI,
+    db: process.env.MONGO_LOG_DB
+  },
+
+  jwtSecret: process.env.JWT_SECRET,
+  logLevel: process.env.LOG_LEVEL || 'info',
+  inDev: process.env.NODE_ENV === 'development'
+};
