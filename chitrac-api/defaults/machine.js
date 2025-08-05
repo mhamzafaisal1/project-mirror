dcd906a299d9b9962ff047613100117bd7d361d7
Robert T Ivaniszyn II <rivaniszyn@chidry.com>
2025-08-05 15:31:09 -0500

    Updated Default Files

--- chitrac-api/defaults/machine.js
+++ chitrac-api/defaults/machine.js
@@ -1,15 +1,15 @@
 module.exports = {
     machine: [
-        { serial: 68011, name: 'SPF1', active: true, ipAddress: '192.168.0.1', lanes: 1 },
-        { serial: 68012, name: 'SPF2', active: true, ipAddress: '192.168.0.2', lanes: 1 },
-        { serial: 68013, name: 'SPF3', active: true, ipAddress: '192.168.0.3', lanes: 1 },
-        { serial: 68014, name: 'SPF4', active: true, ipAddress: '192.168.0.4', lanes: 1 },
-        { serial: 68015, name: 'SPF5', active: true, ipAddress: '192.168.0.5', lanes: 1 },
-        { serial: 68016, name: 'SPF6', active: true, ipAddress: '192.168.0.6', lanes: 1 },
-        { serial: 67798, name: 'LPL1', active: true, ipAddress: '192.168.0.7', lanes: 3 },
-        { serial: 67799, name: 'LPL2', active: true, ipAddress: '192.168.0.8', lanes: 3 },
-        { serial: 67801, name: 'Blanket1', active: true, ipAddress: '192.168.0.9', lanes: 2 },
-        { serial: 67802, name: 'Blanket2', active: true, ipAddress: '192.168.0.10', lanes: 2 },
-        { serial: 67800, name: 'SPL1', active: true, ipAddress: '192.168.0.11', lanes: 4 }
+        { "serial": 90001, "name": "SPF1", "active": true, "ipAddress": "192.168.0.1", "lanes": 1 },
+        { "serial": 90002, "name": "SPF2", "active": true, "ipAddress": "192.168.0.2", "lanes": 1 },
+        { "serial": 90003, "name": "SPF3", "active": true, "ipAddress": "192.168.0.3", "lanes": 1 },
+        { "serial": 90004, "name": "SPF4", "active": true, "ipAddress": "192.168.0.4", "lanes": 1 },
+        { "serial": 90005, "name": "SPF5", "active": true, "ipAddress": "192.168.0.5", "lanes": 1 },
+        { "serial": 90006, "name": "SPF6", "active": true, "ipAddress": "192.168.0.6", "lanes": 1 },
+        { "serial": 90007, "name": "LPL1", "active": true, "ipAddress": "192.168.0.7", "lanes": 3 },
+        { "serial": 90008, "name": "LPL2", "active": true, "ipAddress": "192.168.0.8", "lanes": 3 },
+        { "serial": 90009, "name": "Blanket1", "active": true, "ipAddress": "192.168.0.9", "lanes": 2 },
+        { "serial": 90010, "name": "Blanket2", "active": true, "ipAddress": "192.168.0.10", "lanes": 2 },
+        { "serial": 90011, "name": "SPL1", "active": true, "ipAddress": "192.168.0.11", "lanes": 4 }
     ]
 }
