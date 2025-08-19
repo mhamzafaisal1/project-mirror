b77c1ff34c52c62c25cda37cb74713a26e5d0022
Robert T Ivaniszyn II <rivaniszyn@chidry.com>
2025-08-19 10:00:39 -0500

    luxon fromMillis

--- chitrac-api/controllers/alpha/operatorSessions.js
+++ chitrac-api/controllers/alpha/operatorSessions.js
@@ -38,8 +38,8 @@ module.exports = function (server) {
   router.get("/analytics/operators-summary", async (req, res) => {
     try {
       const { start, end } = parseAndValidateQueryParams(req);
-      const queryStart = new Date(DateTime.fromFormat(start, "yyyy-MM-dd'T'HH:mm").toISO()); //new Date(start); NEED LUXON FOR TIMEZONE ISSUES
-      let queryEnd = new Date(DateTime.fromFormat(end, "yyyy-MM-dd'T'HH:mm").toISO()); //new Date(end);
+      const queryStart = new Date(DateTime.fromMillis(start).toISO()); //new Date(start); NEED LUXON FOR TIMEZONE ISSUES
+      let queryEnd = new Date(DateTime.fromMillis(end).toISO()); //new Date(end);
       const now = new Date(DateTime.now().toISO());
       if (queryEnd > now) queryEnd = now;
       if (!(queryStart < queryEnd)) {
